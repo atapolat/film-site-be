d233e16102e9f9c6508c047a11d55e652440e416
alpayyy <115874416+alpayyy@users.noreply.github.com>
2023-10-29 22:44:40 +0300

    update

--- src/main/java/pinsoft/intern/movieSite/domain/authentication/api/AuthenticationService.java
+++ src/main/java/pinsoft/intern/movieSite/domain/authentication/api/AuthenticationService.java
@@ -1,7 +1,7 @@
 package pinsoft.intern.movieSite.domain.authentication.api;
 
 import pinsoft.intern.movieSite.domain.user.api.UserDto;
-import pinsoft.intern.movieSite.domain.user.api.UserRegisterDto;
+//import pinsoft.intern.movieSite.domain.user.api.UserRegisterDto;
 import pinsoft.intern.movieSite.domain.authentication.impl.AuthenticationResponse;
 import pinsoft.intern.movieSite.domain.authentication.impl.AuthenticationRequest;
 
